6e608b4dbbb4336c7bbe7c546d7681f90e38ca21
kshired <shiroed1211@naver.com>
2021-05-30 23:02:18 +0900

    add time menu

--- weather.js
+++ weather.js
@@ -4,6 +4,10 @@ const { app, Menu, nativeImage } = require('electron');
 
 let myMenu = null;
 
+const getTime = () => {
+  const today = new Date();
+  return today.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+};
 // fetching weather data
 const fetchWeather = async (lat, lon) => {
   const res = await axios.get(process.env.API_URL, {
@@ -28,10 +32,22 @@ const fetchWeather = async (lat, lon) => {
 // get a icon name by weather api, then change icon and menu
 const changeWeather = async (tray, lat, lon, dir) => {
   const { icon, description } = await fetchWeather(lat, lon);
+  const updatedTime = getTime();
 
   myMenu = Menu.buildFromTemplate([
     {
       label: description,
+      type: 'normal',
+    },
+    {
+      type: 'separator',
+    },
+    {
+      label: 'Last Update Time',
+      type: 'normal',
+    },
+    {
+      label: updatedTime,
       type: 'normal',
     },
     {
